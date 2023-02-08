a91532c6ad0c1e78fb61ed96287f94f604314894
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2023-02-08 10:14:12 +0200

    updated file

--- app/src/main/java/com/example/news_articles/articlesApii/APIclient.java
+++ app/src/main/java/com/example/news_articles/articlesApii/APIclient.java
@@ -1,2 +1,32 @@
-package com.example.news_articles.articlesApii;public class APIclient {
+package com.example.news_articles.articlesApii;
+
+
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Retrofit;
+
+public class APIclient {
+
+
+    private static APIclient instance;
+    private static final String BASE_URL = "";
+    private static Retrofit retrofit;
+
+    private  APIclient (){
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public synchronized APIclient getInstance() {
+        if (instance == null) {
+            instance = new APIclient();
+        }
+        return instance;
+    }
+
+    public NewsService getApi(){
+        return retrofit.create(NewsService.class);
+    }
+    
 }
