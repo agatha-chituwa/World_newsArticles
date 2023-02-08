6614310117ef1fbc5243bba9293dad51331b3021
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2023-02-08 22:39:55 +0200

    constants + rxjava2callAdapterFactory

--- app/src/main/java/com/example/news_articles/articlesApii/APIclient.java
+++ app/src/main/java/com/example/news_articles/articlesApii/APIclient.java
@@ -1,6 +1,8 @@
 package com.example.news_articles.articlesApii;
 
+import com.example.news_articles.utils.Constants;
 
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Retrofit;
 
@@ -8,25 +10,28 @@ public class APIclient {
 
 
     private static APIclient instance;
-    private static final String BASE_URL = "";
+
     private static Retrofit retrofit;
+    private NewsService newsService;
+    private Constants constants;
+
 
     private  APIclient (){
         retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(constants.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
+        newsService = retrofit.create(NewsService.class);
     }
 
-    public synchronized APIclient getInstance() {
+    public static APIclient getInstance() {
         if (instance == null) {
             instance = new APIclient();
         }
         return instance;
     }
 
-    public NewsService getApi(){
-        return retrofit.create(NewsService.class);
-    }
+
     
 }
---
+++ app/src/main/java/com/example/news_articles/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.news_articles.utils;
+
+public class Constants {
+    public static final String BASE_URL = "https://newsapi.org/v2/top-headlines";
+
+
+
+}
