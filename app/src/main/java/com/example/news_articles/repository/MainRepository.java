8e419611e70fe3493b1341682d2b18aef24463e5
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2023-02-13 22:30:26 +0200

    to be stashed

---
+++ app/src/main/java/com/example/news_articles/repository/MainRepository.java
@@ -0,0 +1,10 @@
+package com.example.news_articles.repository;
+
+import com.example.news_articles.articlesApii.NewsService;
+
+public class MainRepository {
+    //take the instance of NewsService
+
+    private NewsService newsService;
+
+}
