82d11f36bbc701120ed499131fcd348e642b0752
ASIMWELA <augastinesimwela@gmail.com>
2023-03-26 22:58:59 +0200

    add the article loading mutable live data

--- app/src/main/java/com/example/news_articles/articles/ArticlesViewModel.java
+++ app/src/main/java/com/example/news_articles/articles/ArticlesViewModel.java
@@ -7,10 +7,12 @@ import android.util.Log;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.bumptech.glide.load.engine.Engine;
 import com.example.news_articles.articlesApii.APIclient;
 
 import com.example.news_articles.articlesApii.NewsService;
 import com.example.news_articles.repository.Repository;
+import com.example.news_articles.utils.LoadingStatus;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -19,7 +21,7 @@ import rx.schedulers.Schedulers;
 public class ArticlesViewModel extends ViewModel {
 
 
-    private Repository repository;
+    private final Repository repository;
 
     public ArticlesViewModel(Repository repository){
         this.repository = repository;
@@ -28,51 +30,20 @@ public class ArticlesViewModel extends ViewModel {
 
 
     public MutableLiveData<ArticleNetworkResponse> articles = new MutableLiveData<>();
+    public MutableLiveData<LoadingStatus> loadingStatus = new MutableLiveData<>();
 
 
     public void getArticles() {
+        loadingStatus.postValue(LoadingStatus.LOADING);
         repository.getInstance().getArticles()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(articleNetworkResponse -> {
+                    loadingStatus.postValue(LoadingStatus.FINISHED);
                     articles.postValue(articleNetworkResponse);
                 }, error -> {
+                    loadingStatus.postValue(LoadingStatus.ERROR);
                     Log.e(TAG, error.toString());
                 });
     }
 }
-
-
-
-
-
-//passing the repository as a parameter to the view model constructor
-
-//    private final Repository repository;
-//    public ArticlesViewModel(Repository repository){
-//        this.repository = repository;
-//    }
-//    private MainRepository repository;
-//    public MutableLiveData<ArticleNetworkResponse> articles = new MutableLiveData<>();
-//
-//    public  void getArti() {
-//        repository = new MainRepository();
-//        articles = repository.getArticles();
-//    }
-
-//    private static final String TAG = "ArticlesViewModel";
-//
-//    public MutableLiveData<ArticleNetworkResponse> article = new MutableLiveData<>();
-//
-//    public void getArticles() {
-//        APIclient.getInstance().getApi().getNewsArticles(API_KEY, "us")
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(articleNetworkResponse -> {
-//                    articles.postValue(articleNetworkResponse);
-//
-//                }, error -> {
-//                    Log.e(TAG, error.toString());
-//                });
-//    }
-//}
